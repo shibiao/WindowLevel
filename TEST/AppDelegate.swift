a74ed2a3676741f4a7b97b1e2085ed4c8af28507
WalkWithWind <956035125@qq.com>
2017-11-20 15:07:50 +0800

    Initial Commit

---
+++ TEST/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TEST
+//
+//  Created by sycf_ios on 2017/11/20.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
